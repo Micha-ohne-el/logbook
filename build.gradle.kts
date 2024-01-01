9551884fda2e9220216a0ea2d5f0b4119dbb92bf
Micha Lehmann <michalehmann0112@gmail.com>
2024-01-01 22:04:26 +0100

    Add an integration pipeline using Kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	kotlin("multiplatform") version "1.9.21"
+	id("io.kotest.multiplatform") version "5.8.0"
 }
 
 group = "moe.micha"
@@ -8,6 +9,7 @@ version = "0.0.0"
 
 object Versions {
 	const val datetime = "0.5.0"
+	const val kotest = "5.8.0"
 }
 
 repositories {
@@ -17,6 +19,12 @@ repositories {
 kotlin {
 	jvm {
 		jvmToolchain(8)
+
+		testRuns.all {
+			executionTask.configure {
+				useJUnitPlatform()
+			}
+		}
 	}
 
 	js(IR) {
@@ -32,6 +40,15 @@ kotlin {
 	sourceSets {
 		commonMain.dependencies {
 			implementation("org.jetbrains.kotlinx:kotlinx-datetime:${Versions.datetime}")
+		}
+
+		commonTest.dependencies {
+			implementation("io.kotest:kotest-framework-engine:${Versions.kotest}")
+			implementation("io.kotest:kotest-assertions-core:${Versions.kotest}")
+		}
+
+		jvmTest.dependencies {
+			implementation("io.kotest:kotest-runner-junit5:${Versions.kotest}")
 		}
 	}
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 [![Kotlin 1.9.21](https://img.shields.io/badge/Kotlin%2FMultiplatform-1.9.21-7F52FF.svg?logo=kotlin)](http://kotlinlang.org)
+[![CI status badge](https://img.shields.io/github/actions/workflow/status/Micha-ohne-el/logbook/integration.yaml?label=CI)](https://github.com/Micha-ohne-el/logbook/actions/workflows/integration.yaml)
 [![License – MIT](https://img.shields.io/github/license/Micha-ohne-el/logbook?color=gold&label=License)](https://github.com/Micha-ohne-el/logbook/blob/main/license.md)
 
 # logbook
