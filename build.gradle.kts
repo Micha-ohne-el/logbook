2f10936f3258e1063e195f7a5b7d61374e5ef8df
Micha Lehmann <michalehmann0112@gmail.com>
2024-01-02 02:03:02 +0100

    Add everything needed to publish to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
+import java.net.URL
+import org.jetbrains.dokka.gradle.DokkaTask
+
 plugins {
 	kotlin("multiplatform") version "1.9.21"
 	id("io.kotest.multiplatform") version "5.8.0"
+	id("org.jetbrains.dokka") version "1.9.10"
+
+	id("maven-publish")
+	id("signing")
 }
 
 group = "moe.micha"
@@ -51,4 +58,110 @@ kotlin {
 			implementation("io.kotest:kotest-runner-junit5:${Versions.kotest}")
 		}
 	}
+}
+
+val sonatypeUsername: String? = System.getenv("SONATYPE_USERNAME")
+val sonatypePassword: String? = System.getenv("SONATYPE_PASSWORD")
+val gpgPrivateKey: String? = System.getenv("GPG_PRIVATE_KEY")
+val gpgKeyPassword: String? = System.getenv("GPG_KEY_PASSWORD")
+
+val repositoryBranch: String? = System.getenv("GITHUB_REF_NAME")
+
+val repositoryUrl: String? = run {
+	val githubServerUrl: String? = System.getenv("GITHUB_SERVER_URL")
+	val githubRepository: String? = System.getenv("GITHUB_REPOSITORY")
+
+	if (githubServerUrl == null || githubRepository == null) {
+		null
+	} else {
+		"$githubServerUrl/$githubRepository"
+	}
+}
+
+val dokkaOutputDir = layout.buildDirectory.get().dir("dokka").dir("html")
+
+tasks.withType<DokkaTask>().configureEach {
+	outputDirectory = dokkaOutputDir
+
+	dokkaSourceSets.configureEach {
+		sourceLink {
+			localDirectory = projectDir.resolve("src")
+			remoteUrl = URL("$repositoryUrl/tree/$repositoryBranch/src")
+			remoteLineSuffix = "#L"
+		}
+	}
+}
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+	archiveClassifier = "javadoc"
+
+	from(dokkaOutputDir)
+}
+
+tasks.withType<AbstractPublishToMaven> {
+	dependsOn(tasks.withType<Sign>())
+}
+
+publishing {
+	repositories {
+		maven {
+			name = "oss"
+			val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+			val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+
+			url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+			credentials {
+				username = sonatypeUsername
+				password = sonatypePassword
+			}
+		}
+	}
+
+	publications.withType<MavenPublication> {
+		artifact(javadocJar)
+
+		pom {
+			name = project.name
+			description = project.description
+			url = repositoryUrl
+
+			licenses {
+				license {
+					name = "MIT"
+					url = "$repositoryUrl/blob/$repositoryBranch/license.md"
+				}
+			}
+
+			issueManagement {
+				system = "GitHub"
+				url = "$repositoryUrl/issues"
+			}
+
+			scm {
+				connection = "$repositoryUrl.git"
+				url = repositoryUrl
+			}
+
+			developers {
+				developer {
+					name = "Micha Lehmann"
+					email = "michalehmann0112@gmail.com"
+				}
+			}
+		}
+	}
+}
+
+signing {
+	setRequired { // avoid having to sign local publications
+		!gradle.taskGraph.hasTask(":publishToMavenLocal")
+	}
+
+	useInMemoryPgpKeys(
+		gpgPrivateKey,
+		gpgKeyPassword,
+	)
+
+	sign(publishing.publications)
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 [![Kotlin 1.9.21](https://img.shields.io/badge/Kotlin%2FMultiplatform-1.9.21-7F52FF.svg?logo=kotlin)](http://kotlinlang.org)
+[![Maven Central](https://img.shields.io/maven-central/v/moe.micha/logbook?label=Latest%20Version)](https://central.sonatype.com/artifact/moe.micha/logbook)
 [![CI status badge](https://img.shields.io/github/actions/workflow/status/Micha-ohne-el/logbook/integration.yaml?label=CI)](https://github.com/Micha-ohne-el/logbook/actions/workflows/integration.yaml)
+[![CD status badge](https://img.shields.io/github/actions/workflow/status/Micha-ohne-el/logbook/deployment.yaml?label=CD)](https://github.com/Micha-ohne-el/logbook/actions/workflows/deployment.yaml)
+[![Documentation available](https://img.shields.io/badge/Documentation-available-blue)](https://logbook.micha.moe)
 [![License – MIT](https://img.shields.io/github/license/Micha-ohne-el/logbook?color=gold&label=License)](https://github.com/Micha-ohne-el/logbook/blob/main/license.md)
 
 # logbook
