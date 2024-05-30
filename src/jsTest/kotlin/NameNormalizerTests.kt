b936785b46c81f1859d91f454dd911dd187a2c0f
Micha Lehmann <michalehmann0112@gmail.com>
2024-05-30 21:42:39 +0200

    fix: Add missing tests

--- src/jsTest/kotlin/NameNormalizerTests.kt
+++ src/jsTest/kotlin/NameNormalizerTests.kt
@@ -13,6 +13,28 @@ class NameNormalizerTests : DescribeSpec({
 
 			defaultNameNormalizer(kClass) shouldBe null
 		}
+
+		it("returns simpleName name") {
+			val kClass = Logbook::class.mock(simpleName = "Test")
+
+			defaultNameNormalizer(kClass) shouldBe "Test"
+		}
+
+		it("strips suffixes off simpleName") {
+			val kClass1 = Logbook::class.mock(simpleName = "TestLog")
+			val kClass2 = Logbook::class.mock(simpleName = "TestLogger")
+			val kClass3 = Logbook::class.mock(simpleName = "TestLogbook")
+
+			defaultNameNormalizer(kClass1) shouldBe "Test"
+			defaultNameNormalizer(kClass2) shouldBe "Test"
+			defaultNameNormalizer(kClass3) shouldBe "Test"
+		}
+
+		it("converts simpleName to title case") {
+			val kClass = Logbook::class.mock(simpleName = "test")
+
+			defaultNameNormalizer(kClass) shouldBe "Test"
+		}
 	}
 })
 
